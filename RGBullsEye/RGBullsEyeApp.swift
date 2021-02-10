aa9076367769bc34efaf67a30419afb4300ae312
Wassim MANSOURI <wassim.mansouri@redfabriq.com>
2021-02-10 07:13:11 +0100

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Wassim Mansouri on 10/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
