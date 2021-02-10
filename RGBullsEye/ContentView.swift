551272831de92d0486a516afddd89244d58e41f2
Wassim MANSOURI <wassim.mansouri@redfabriq.com>
2021-02-10 15:57:18 +0100

    Finalize app
    Finalize the sliders and the overall app behavior.
    
    Co-Authored-By: Wassim Mansouri <1204255+wassimans@users.noreply.github.com>

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -8,15 +8,73 @@
 import SwiftUI
 
 struct  ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    @State var showAlert = false
+    
+    func computeScore() -> Int {
+      let rDiff = rGuess - rTarget
+      let gDiff = gGuess - gTarget
+      let bDiff = bGuess - bTarget
+      let diff = sqrt((rDiff * rDiff + gDiff * gDiff
+        + bDiff * bDiff) / 3.0)
+      return lround((1.0 - diff) * 100.0)
+    }
+
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                        .padding()
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))" + " G: \(Int(gGuess * 255.0))" + " B: \(Int(bGuess * 255.0))")
+                        .padding()
+                }
+            }
+            Button(action: { self.showAlert = true }) {
+              Text("Hit Me!")
+            }
+            .alert(isPresented: $showAlert) {
+              Alert(title: Text("Your Score"),
+                message: Text(String(computeScore())))
+            }
             .padding()
+
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.2, gGuess: 0.8, bGuess: 0.3)
             .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+
+    var body: some View {
+        HStack {
+            Text("0")
+                .foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255")
+                .foregroundColor(textColor)
+        }
+        .padding(.horizontal)
     }
 }
